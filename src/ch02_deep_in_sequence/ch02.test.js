8f445ffde577e647cec40d6cfc97e0a2fa09efa8
Paul Gualotuna <p.gualotuna@celonis.de>
2020-03-21 20:22:56 +0100

    chore(tdd): add minimal tdd environment

---
+++ src/ch02_deep_in_sequence/ch02.test.js
@@ -0,0 +1,8 @@
+import {expect} from "chai";
+
+describe("Deep in the Sequence", () => {
+
+    it("should return true", () => {
+        expect(1).equal(1);
+    })
+});
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { Observable } from "rxjs";
-
-Observable.of("Hello RxJS World!").subscribe(result => {
-    let text = document.createTextNode(result);
-    document.body.appendChild(text);
-});
